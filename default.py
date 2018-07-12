2e11e85217d65e3f00e85d7b05b99e62e3edf11f
manish arya <montiarya555@gmail.com>
2018-07-12 22:10:03 +0530

    using class concept for spy details by replacing dictionary

--- default.py
+++ default.py
@@ -3,12 +3,30 @@
 # spy_age = 21
 # spy_rating = 4.5
 
-from test import Name ,Class
-print (Name,Class)
+# from test import Name ,Class
+# print (Name,Class)
 
- spy = {'name' : 'Bond',
-        'salutation' : 'Mr.',
-        'age' : 23,
-        'rating' : 4.5,
-        'Online Status' : True
-       }
+# spy = {'name' : 'Bond',
+#         'salutation' : 'Mr.',
+#        'age' : 23,
+#        'rating' : 4.5,
+#        'Online Status' : True
+#       }
+
+
+class Spy:
+    def __init__(self,name,salutation,age,rating):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.online_status = True
+        self.current_status_msg = None
+
+spy = Spy("Bond" , "Mr." , 25, 4.3)
+
+friend_1 = Spy("Monti" , "Mr." , 21, 3.0)
+friend_2 = Spy("Deepak" , "Mr." , 19, 4.4)
+friend_3 = Spy("Rohit" , "Mr." , 45, 4.1)
+
+friend = [friend_1,friend_2,friend_3]
